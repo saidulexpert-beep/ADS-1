ac0ece0b23b73c0835e946dd877950602706dcd2
saidulexpert-beep <saidulexpert@gmail.com>
2026-09-25 08:40:14 +0600

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   }
 
   // 🔴 এখানে আপনার NagorikPay মার্চেন্ট প্যানেলের লাইভ API Key বসান
-  const API_KEY = 'rmeJ0DKDg3Q60tWUEjK1rNkgFZngPw1xye49FqBuYWnf977ojS';
+  const API_KEY = 'hRECFjf99Y57Gl0wcs2pjeVaEWDPZCqU5WL85uYmaU5EVdEY3m';
   const BASE_URL = 'https://secure-pay.nagorikpay.com/api/payment';
   
   const action = req.query.action;
